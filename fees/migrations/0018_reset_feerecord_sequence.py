4c78127fa7d2ec9dd83dd29e74f3735398c48ac3
LUHAN <YOUR-GITHUB-EMAIL@example.com>
2026-09-26 02:00:13 +0300

    Fix FeeRecord ID sequence

---
+++ fees/migrations/0018_reset_feerecord_sequence.py
@@ -0,0 +1,28 @@
+﻿from django.db import migrations
+from django.core.management.color import no_style
+
+
+def reset_feerecord_sequence(apps, schema_editor):
+    FeeRecord = apps.get_model("fees", "FeeRecord")
+
+    sql_list = schema_editor.connection.ops.sequence_reset_sql(
+        no_style(),
+        [FeeRecord],
+    )
+
+    with schema_editor.connection.cursor() as cursor:
+        for sql in sql_list:
+            cursor.execute(sql)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("fees", "0017_mpesapaybilltransaction"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            reset_feerecord_sequence,
+            migrations.RunPython.noop,
+        ),
+    ]
